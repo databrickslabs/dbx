c35f588626cda7ba06a2ef14fa9fddb564aca443
renardeinside <polarpersonal@gmail.com>
2020-08-06 22:56:50 +0200

    add initial coverage

---
+++ dbx/template/{{cookiecutter.project_name}}/config/scripts/providers.libsonnet
@@ -0,0 +1,7 @@
+{
+  cluster(env, job) :: {},
+  name(env, job) :: {},
+  pythonFile(env, job) :: {},
+  jobArguments(env, job) :: [],
+  parameters(env, job) :: {}
+}
--- tests/test_dbx_azure.py
+++ tests/test_dbx_azure.py
@@ -2,20 +2,38 @@ import os
 import unittest
 
 from click.testing import CliRunner
+from dotenv import load_dotenv
 
 from dbx.cli.init import init
 
+# fixes the security if launch is not inside pipeline
+if not os.environ.get("AZURE_DATABRICKS_TOKEN"):
+    load_dotenv()
 
-class DbxTest(unittest.TestCase):
+
+class DbxAzureTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        pass
+
     def test_init(self):
         runner = CliRunner()
         project_name = "dbx-test"
         args = [
             "--project-name", project_name,
-            "--cloud", "AWS",
+            "--cloud", "Azure",
             "--pipeline-engine", "GitHub Actions"
         ]
         with runner.isolated_filesystem():
             result = runner.invoke(init, args)
             self.assertFalse(result.exception)
             self.assertTrue(os.path.exists(project_name))
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
