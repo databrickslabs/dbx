524868316f68ac82de26f996c45ca079302ee463
renardeinside <polarpersonal@gmail.com>
2020-10-08 23:18:58 +0200

    first implementation of watchdog with tunnel capabilities

--- dbx/cli/dev/watchdog.py
+++ dbx/cli/dev/watchdog.py
@@ -7,10 +7,17 @@ from databricks_cli.cli import debug_option
 from databricks_cli.clusters.api import ClusterService
 from databricks_cli.sdk.api_client import ApiClient
 from databricks_cli.utils import CONTEXT_SETTINGS
-
-from dbx.cli.execute import _preprocess_cluster_args
+import pathlib
+import sys
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
+from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+import paramiko
+import os
+from typing import Tuple
+from dbx.cli.execute import _preprocess_cluster_args, execute_command
 from dbx.utils.common import (
-    environment_option, dbx_echo, prepare_environment, ApiV1Client, ContextLockFile
+    environment_option, dbx_echo, prepare_environment, ApiV1Client, ContextLockFile, TunnelInfo, get_ssh_client
 )
 
 COMMANDS = {
@@ -69,7 +76,7 @@ class ContextManager:
         self._api_v1_client = api_v1_client
         self._cluster_manager = cluster_manager
         self._status = "initializing"
-        self._context_id = ContextLockFile.get_context()
+        self.context_id = ContextLockFile.get_context()
 
     @property
     def status(self):
@@ -82,14 +89,14 @@ class ContextManager:
         return response["id"]
 
     async def _get_context_status_info(self):
-        payload = {"clusterId": self._cluster_manager.cluster_id, "contextId": self._context_id}
+        payload = {"clusterId": self._cluster_manager.cluster_id, "contextId": self.context_id}
         status_info = self._api_v1_client.get_context_status(payload)
         return status_info
 
     async def _creation_routine(self):
         self._status = "creating a new context"
-        self._context_id = await self._create_context()
-        ContextLockFile.set_context(self._context_id)
+        self.context_id = await self._create_context()
+        ContextLockFile.set_context(self.context_id)
         await asyncio.sleep(5)
 
     async def context_routine(self):
@@ -98,7 +105,7 @@ class ContextManager:
                 self._status = "waiting for the cluster"
                 await asyncio.sleep(5)
             else:
-                if not self._context_id:
+                if not self.context_id:
                     await self._creation_routine()
                 else:
                     self._status = "verifying if existing context is active"
@@ -119,8 +126,107 @@ class ContextManager:
                                 await asyncio.sleep(5)
                             else:
                                 self._status = "existing context is not active, creating a new one"
-                                self._context_id = None
+                                self.context_id = None
                                 await asyncio.sleep(3)
+
+
+class TunnelManager:
+    def __init__(self, api_v1_client: ApiV1Client, cluster_id: str, context_manager: ContextManager):
+        self._api_v1_client = api_v1_client
+        self._context_manager = context_manager
+        self._status = "initializing"
+        self._cluster_id = cluster_id
+        self._url = ContextLockFile.get_url()
+        self._private_key_file = pathlib.Path("~/.ssh/%s" % self._cluster_id).expanduser()
+
+    def _exec(self, cmd, verbose=False):
+        return execute_command(self._api_v1_client, self._cluster_id, self._context_manager.context_id, cmd, verbose)
+
+    @property
+    def status(self):
+        return self._status
+
+    def get_tunnel_info(self) -> TunnelInfo:
+        (host, port) = self._url.replace('tcp://', "").split(":")
+        return TunnelInfo(host, int(port), str(self._private_key_file))
+
+    def _prepare_sshd(self):
+        client = get_ssh_client(self.get_tunnel_info())
+        client.exec_command("mkdir -p /usr/lib/ssh")
+        client.exec_command("ln -s /usr/lib/openssh/sftp-server /usr/lib/ssh/sftp-server")
+        client.exec_command("systemctl restart ssh.service")
+
+    async def tunnel_routine(self):
+        while True:
+            if not self._context_manager.status == "running":
+                self._status = "waiting for the context"
+                await asyncio.sleep(5)
+            else:
+                if self._url:
+                    self._status = "checking cached tunnel url"
+                    try:
+                        await self._check_tunnel()
+                        self._status = "running"
+                        await asyncio.sleep(5)
+                    except (paramiko.ssh_exception.AuthenticationException,
+                            paramiko.ssh_exception.NoValidConnectionsError,
+                            FileNotFoundError):
+                        self._status = "tunnel is unreachable, initializing a new one"
+                        await self.initialize_tunnel()
+                    await asyncio.sleep(5)
+                else:
+                    self._status = "initializing a plain new tunnel"
+                    await self.initialize_tunnel()
+                    await asyncio.sleep(5)
+
+    async def initialize_tunnel(self):
+        self._exec(COMMANDS["install_libraries"])
+        self._exec(COMMANDS["stop_ngrok"])
+
+        private_key, public_key = await self.generate_key_pair()
+
+        if self._private_key_file.exists():
+            self._private_key_file.unlink()
+
+        self._private_key_file.write_bytes(private_key)
+        os.chmod(self._private_key_file, 0o600)
+
+        remote_keys_cmd = COMMANDS['install_ssh_keys'].format(
+            private_key=private_key.decode('utf-8'),
+            public_key=public_key.decode('utf-8')
+        )
+
+        self._exec(remote_keys_cmd, verbose=False)
+        self._exec(COMMANDS['generate_url'].format(token=os.environ["DBX_NGROK_TOKEN"]))
+
+        url = self._exec(COMMANDS['print_url'])
+        ContextLockFile.set_url(url)
+        self._url = url
+        self._prepare_sshd()
+
+    async def _check_tunnel(self):
+        client = get_ssh_client(self.get_tunnel_info())
+        client.exec_command("ls -la")
+
+    @staticmethod
+    async def generate_key_pair() -> Tuple[bytes, bytes]:
+        key = rsa.generate_private_key(
+            backend=crypto_default_backend(),
+            public_exponent=65537,
+            key_size=2048
+        )
+
+        private_key = key.private_bytes(
+            crypto_serialization.Encoding.PEM,
+            crypto_serialization.PrivateFormat.TraditionalOpenSSL,
+            crypto_serialization.NoEncryption()
+        )
+
+        public_key = key.public_key().public_bytes(
+            crypto_serialization.Encoding.OpenSSH,
+            crypto_serialization.PublicFormat.OpenSSH
+        )
+        return private_key, public_key
 
 
 class DevApp:
@@ -133,10 +239,12 @@ class DevApp:
 
     cluster_status_widget = ur.Text("")
     context_status_widget = ur.Text("")
+    tunnel_status_widget = ur.Text("")
 
-    status_widget = ur.Columns([
+    status_widget = ur.Pile([
         ur.LineBox(cluster_status_widget),
         ur.LineBox(context_status_widget),
+        ur.LineBox(tunnel_status_widget)
     ])
 
     main_widget = ur.Filler(ur.Pile([
@@ -148,9 +256,13 @@ class DevApp:
                  environment: str,
                  cluster_manager: ClusterManager,
                  context_manager: ContextManager,
+                 tunnel_manager: TunnelManager,
                  port: int = 4004):
+
         self._cluster_manager = cluster_manager
         self._context_manager = context_manager
+        self._tunnel_manager = tunnel_manager
+
         self._port = port
         self._environment = environment
         self._asyncio_loop = asyncio.get_event_loop()
@@ -158,10 +270,12 @@ class DevApp:
         self._header_task = self._asyncio_loop.create_task(self._header_handler())
 
         self._cluster_routine_task = self._asyncio_loop.create_task(self._cluster_manager.cluster_routine())
-        self._cluster_status_task = self._asyncio_loop.create_task(self._cluster_status_handler())
-
         self._context_routine_task = self._asyncio_loop.create_task(self._context_manager.context_routine())
+        self._tunnel_routine_task = self._asyncio_loop.create_task(self._tunnel_manager.tunnel_routine())
+
+        self._cluster_status_task = self._asyncio_loop.create_task(self._cluster_status_handler())
         self._context_status_task = self._asyncio_loop.create_task(self._context_status_handler())
+        self._tunnel_status_task = self._asyncio_loop.create_task(self._tunnel_status_handler())
 
         self._server_coroutine = asyncio.start_server(self._server_routine, '127.0.0.1', 4004, loop=self._asyncio_loop)
         self._server = self._asyncio_loop.run_until_complete(self._server_coroutine)
@@ -172,10 +286,12 @@ class DevApp:
         self._ur_main_loop.watch_pipe(self._update_header)
         self._ur_main_loop.watch_pipe(self._update_cluster_status)
         self._ur_main_loop.watch_pipe(self._update_context_status)
+        self._ur_main_loop.watch_pipe(self._update_tunnel_status)
 
         self._update_header()
         self._update_cluster_status()
         self._update_context_status()
+        self._update_tunnel_status()
 
     async def _server_routine(self, _, reader):
         pass
@@ -195,11 +311,24 @@ class DevApp:
             await asyncio.sleep(5)
             self._update_context_status()
 
+    async def _tunnel_status_handler(self):
+        while True:
+            await asyncio.sleep(5)
+            self._update_tunnel_status()
+
     def _update_context_status(self):
         self.context_status_widget.set_text(f"Context status: {self._context_manager.status}")
 
     def _update_cluster_status(self):
         self.cluster_status_widget.set_text(f"Cluster status: {self._cluster_manager.status}")
+
+    def _update_tunnel_status(self):
+        if self._tunnel_manager.status == "running":
+            info = self._tunnel_manager.get_tunnel_info()
+            msg = f"Tunnel status: {self._tunnel_manager.status} @ {info.host}:{info.port}"
+            self.tunnel_status_widget.set_text(msg)
+        else:
+            self.tunnel_status_widget.set_text(f"Tunnel status: {self._tunnel_manager.status}")
 
     @staticmethod
     def _current_dttm():
@@ -224,16 +353,27 @@ class DevApp:
         self._header_task.cancel()
 
         self._cluster_status_task.cancel()
-        self._cluster_routine_task.cancel()
-
         self._context_status_task.cancel()
+        self._tunnel_status_task.cancel()
+
+        self._cluster_routine_task.cancel()
         self._context_routine_task.cancel()
+        self._tunnel_routine_task.cancel()
 
         self._server.close()
 
         self._asyncio_loop.run_until_complete(self._server.wait_closed())
 
         self._asyncio_loop.close()
+
+
+def check_ngrok_env():
+    if not os.environ.get("DBX_NGROK_TOKEN"):
+        dbx_echo("""Current implementation of dbx dev supports only ngrok-based tunnels.
+        please obtain a token from ngrok.com and set it as an environment variable:
+            export DBX_NGROK_TOKEN='{your token here}' 
+        """)
+        sys.exit(1)
 
 
 @click.command(context_settings=CONTEXT_SETTINGS,
@@ -245,6 +385,7 @@ class DevApp:
 def watchdog(environment: str,
              cluster_id: str,
              cluster_name: str):
+    check_ngrok_env()
     dbx_echo("Starting watchdog in environment %s" % environment)
 
     api_client = prepare_environment(environment)
@@ -254,6 +395,6 @@ def watchdog(environment: str,
 
     cluster_manager = ClusterManager(api_client, cluster_id)
     context_manager = ContextManager(api_v1_client, cluster_manager)
-
-    app = DevApp(environment, cluster_manager, context_manager)
+    tunnel_manager = TunnelManager(api_v1_client, cluster_id, context_manager)
+    app = DevApp(environment, cluster_manager, context_manager, tunnel_manager)
     app.launch()
