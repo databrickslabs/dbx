ebad4f5f9039731b8f041385b2f52d003f9f3085
renardeinside <polarpersonal@gmail.com>
2021-03-16 14:31:29 +0100

    add pywin installation for win32

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
 from setuptools import find_packages, setup
+import sys
 
 from dbx import __version__
 
 with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+INSTALL_REQUIRES = [
+    "path>=15.0.0",
+    "databricks-cli>=0.12.2",
+    "click>=7.1.2",
+    "retry>=0.9.2",
+    "requests>=2.24.0",
+    "mlflow>=1.11.0",
+    "tqdm>=4.50.0",
+    "azure-identity>=1.5.0",
+    "azure-mgmt-datafactory>=1.0.0",
+    "azure-mgmt-subscription>=1.0.0",
+]
+
+if sys.platform.startswith("win32"):
+    INSTALL_REQUIRES.append("pywin32")
+
 setup(
     name="dbx",
     packages=find_packages(exclude=["tests", "tests.*"]),
     setup_requires=["wheel"],
-    install_requires=[
-        "path>=15.0.0",
-        "databricks-cli>=0.12.2",
-        "click>=7.1.2",
-        "retry>=0.9.2",
-        "requests>=2.24.0",
-        "mlflow>=1.11.0",
-        "tqdm>=4.50.0",
-        "azure-identity>=1.5.0",
-        "azure-mgmt-datafactory>=1.0.0",
-        "azure-mgmt-subscription>=1.0.0",
-    ],
+    install_requires=INSTALL_REQUIRES,
     entry_points="""
         [console_scripts]
         dbx=dbx.cli:cli
