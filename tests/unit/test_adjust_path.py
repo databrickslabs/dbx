c3777fee1d92d73c0de801b6d84c598b986da2a3
Sai Jonnala <jspreddy@users.noreply.github.com>
2021-08-03 21:47:35 -0400

    fixing false positive test

--- tests/unit/test_adjust_path.py
+++ tests/unit/test_adjust_path.py
@@ -4,13 +4,18 @@ import json
 from unittest.mock import MagicMock
 import glob
 from pathlib import Path
+import os
 
 
 class AdjustPathTest(unittest.TestCase):
     def test_single_node(self):
-        for file in glob.glob("../deployment-configs/*.json"):
+        # required for when pytest is NOT ran from within the tests/unit dir.
+        glob_path = os.path.join(os.path.dirname(__file__), "../deployment-configs/*.json")
+
+        for file in glob.glob(glob_path):
             raw_conf = Path(file).read_text()
-            deployment = {"jobs": [json.loads(raw_conf)]}
+            deployment_config = json.loads(raw_conf)
+            deployment = deployment_config["default"]
             artifact_base_uri = "dbfs:/fake/test"
             requirements_payload = []
             package_requirement = []
@@ -22,6 +27,7 @@ class AdjustPathTest(unittest.TestCase):
                 requirements_payload,
                 package_requirement,
                 _file_uploader,
+                api_client,
             )
 
             for job_spec in deployment.get("jobs"):
