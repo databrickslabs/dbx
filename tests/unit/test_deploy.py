c3162549112a319a0787660e7abcdbf0d939ac59
renardeinside <polarpersonal@gmail.com>
2021-09-11 12:42:45 +0200

    apply formatting

--- tests/unit/test_deploy.py
+++ tests/unit/test_deploy.py
@@ -124,9 +124,7 @@ class DeployTest(DbxTest):
                 deploy_result = invoke_cli_runner(
                     deploy, ["--environment", "default", "--write-specs-to-file", ".dbx/deployment-result.json"]
                 )
-                _content = json.loads(
-                    pathlib.Path(".dbx/deployment-result.json").read_text()
-                )
+                _content = json.loads(pathlib.Path(".dbx/deployment-result.json").read_text())
                 self.assertNotIn("libraries", _content["default"]["jobs"][0])
                 self.assertIn("libraries", _content["default"]["jobs"][0]["tasks"][0])
                 self.assertEqual(deploy_result.exit_code, 0)
@@ -185,9 +183,7 @@ class DeployTest(DbxTest):
                         ".dbx/deployment-result.json",
                     ],
                 )
-                _content = json.loads(
-                    pathlib.Path(".dbx/deployment-result.json").read_text()
-                )
+                _content = json.loads(pathlib.Path(".dbx/deployment-result.json").read_text())
                 self.assertNotIn("libraries", _content["default"]["jobs"][0])
                 self.assertIn("libraries", _content["default"]["jobs"][0]["tasks"][0])
                 self.assertEqual(deploy_result.exit_code, 0)
