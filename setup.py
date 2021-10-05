09305dc7377179e65e2d548ccf08357de02d42ec
renardeinside <polarpersonal@gmail.com>
2021-10-05 09:01:26 +0200

    add version fixes

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ INSTALL_REQUIRES = [
     "azure-mgmt-datafactory>=1.0.0",
     "azure-mgmt-subscription>=1.0.0",
     "ruamel.yaml>=0.17.10",
+    "cryptography<4.0.0,>=3.3.1",
 ]
 
 if sys.platform.startswith("win32"):
