b042b7acd30d752691c1684075ec26233663cc7a
renardeinside <polarpersonal@gmail.com>
2022-05-05 14:42:52 +0200

    fix tests

--- dbx/templates/projects/python_basic/render/{{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/workloads/sample_etl_job.py
+++ dbx/templates/projects/python_basic/render/{{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/workloads/sample_etl_job.py
@@ -22,3 +22,7 @@ class SampleETLJob(Workload):
 def entrypoint():  # pragma: no cover
     job = SampleETLJob()
     job.launch()
+
+# if you're using spark_python_task, you'll need the __main__ block to start the code execution
+if __name__ == '__main__':
+    entrypoint()
