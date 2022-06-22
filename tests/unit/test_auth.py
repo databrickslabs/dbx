addc9cd42670e9d4bce6bdf615d555d636fb81fc
renardeinside <polarpersonal@gmail.com>
2022-06-22 15:42:25 +0200

    fix lru cache

--- tests/unit/test_auth.py
+++ tests/unit/test_auth.py
@@ -7,14 +7,20 @@ from pytest_mock import MockFixture
 from dbx.api.auth import AuthConfigProvider
 
 
-def test_auth_profile_non_existent(mocker: MockFixture):
+@pytest.fixture()
+def cleanup_auth_cache():
+    method = AuthConfigProvider.get_config
+    method.__func__.cache_clear()
+
+
+def test_auth_profile_non_existent(mocker: MockFixture, cleanup_auth_cache):
     profile_name = "test-non-existent-profile"
     mocker.patch.dict("os.environ", {AuthConfigProvider.DBX_PROFILE_ENV: profile_name}, clear=True)
     with pytest.raises(Exception):
         AuthConfigProvider.get_config()
 
 
-def test_auth_profile_positive(mocker: MockFixture):
+def test_auth_profile_positive(mocker: MockFixture, cleanup_auth_cache):
     profile_name = "test-existent-profile"
     mocker.patch.dict("os.environ", {AuthConfigProvider.DBX_PROFILE_ENV: profile_name}, clear=True)
     expected_config = DatabricksConfig(host="https://some-host", token="dbapiAAABBB", username=None, password=None)
@@ -28,7 +34,7 @@ def test_auth_profile_positive(mocker: MockFixture):
     assert expected_config.token == returned_config.token
 
 
-def test_auth_env_positive(mocker: MockFixture):
+def test_auth_env_positive(mocker: MockFixture, cleanup_auth_cache):
     expected_config = DatabricksConfig(
         host="https://some-other-host", token="dbapiAAABBB", username=None, password=None
     )
@@ -40,7 +46,7 @@ def test_auth_env_positive(mocker: MockFixture):
     assert expected_config.token == returned_config.token
 
 
-def test_auth_both(mocker: MockFixture):
+def test_auth_both(mocker: MockFixture, cleanup_auth_cache):
     expected_env_config = DatabricksConfig(
         host="https://some-env-based-host", token="dbapiAAABBB", username=None, password=None
     )
@@ -68,7 +74,7 @@ def test_auth_both(mocker: MockFixture):
     assert expected_env_config.token == returned_config.token
 
 
-def test_auth_none():
+def test_auth_none(cleanup_auth_cache):
     with pytest.raises(Exception):
         AuthConfigProvider.get_config()
 
