495851a0804b35b5ba24f953f9a7f73af26953b0
renardeinside <polarpersonal@gmail.com>
2022-07-19 01:39:47 +0200

    add no-deps

--- dbx/utils/common.py
+++ dbx/utils/common.py
@@ -89,7 +89,7 @@ def handle_package(rebuild_arg):
             for _file in dist_path.glob("*.whl"):
                 _file.unlink()
 
-        subprocess.check_call([sys.executable] + shlex.split("-m pip wheel -w dist -e . --prefer-binary"))
+        subprocess.check_call([sys.executable] + shlex.split("-m pip wheel -w dist -e . --prefer-binary --no-deps"))
         dbx_echo("Package re-build finished")
 
 
