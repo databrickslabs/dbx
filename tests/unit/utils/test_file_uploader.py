e9b5d9510ac6881f3d636b850720dd688a986091
renardeinside <polarpersonal@gmail.com>
2022-08-04 18:26:01 +0200

    add context uploader tests

--- tests/unit/utils/test_file_uploader.py
+++ tests/unit/utils/test_file_uploader.py
@@ -1,15 +1,15 @@
 from pathlib import PurePosixPath, PureWindowsPath, Path
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 import pytest
 
-from dbx.utils.file_uploader import MlflowFileUploader
+from dbx.utils.file_uploader import MlflowFileUploader, ContextBasedUploader
 
 TEST_ARTIFACT_PATHS = ["s3://some/prefix", "dbfs:/some/prefix", "adls://some/prefix", "gs://some/prefix"]
 
 
 @patch("mlflow.log_artifact", return_value=None)
-def test_uploader(_):
+def test_mlflow_uploader(_):
     local_paths = [PurePosixPath("/some/local/file"), PureWindowsPath("C:\\some\\file")]
 
     for artifact_uri in TEST_ARTIFACT_PATHS:
@@ -18,6 +18,19 @@ def test_uploader(_):
             resulting_path = uploader.upload_and_provide_path(local_path)
             expected_path = "/".join([artifact_uri, str(local_path.as_posix())])
             assert expected_path == resulting_path
+
+
+def test_context_uploader():
+    local_paths = [PurePosixPath("/some/local/file"), PureWindowsPath("C:\\some\\file")]
+    client = MagicMock()
+    base_uri = "/tmp/some/path"
+    client.get_temp_dir = MagicMock(return_value=base_uri)
+
+    for local_path in local_paths:
+        uploader = ContextBasedUploader(client)
+        resulting_path = uploader.upload_and_provide_path(local_path)
+        expected_path = "/".join([base_uri, str(local_path.as_posix())])
+        assert expected_path == resulting_path
 
 
 @patch("mlflow.log_artifact", return_value=None)
