db39cb8bebb0ed7e87aba2b84fa62352878272c5
renardeinside <polarpersonal@gmail.com>
2022-08-17 00:07:52 +0200

    remove version test

--- tests/unit/utils/test_entrypoint.py
+++ tests/unit/utils/test_entrypoint.py
@@ -6,8 +6,3 @@ import sys
 def test_main_version():
     result = subprocess.check_call([sys.executable] + shlex.split("-m dbx --version"))
     assert result == 0
-
-
-def test_main_help():
-    result = subprocess.check_call([sys.executable] + shlex.split("-m dbx --help"))
-    assert result == 0
