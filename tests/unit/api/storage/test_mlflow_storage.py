d307a7f71c174f3098334c71558a516a3092fa30
renardeinside <polarpersonal@gmail.com>
2022-08-19 22:19:13 +0200

    fix experiment setup test

--- tests/unit/api/storage/test_mlflow_storage.py
+++ tests/unit/api/storage/test_mlflow_storage.py
@@ -1,4 +1,6 @@
-import mlflow
+from unittest.mock import MagicMock
+
+from pytest_mock import MockerFixture
 
 from dbx.api.storage.mlflow_based import MlflowStorageConfigurationManager
 from dbx.models.project import EnvironmentInfo, MlflowStorageProperties
@@ -9,12 +11,15 @@ def test_url_strip():
     assert _stripped == "https://some-location.com"
 
 
-def test_experiment_setup(tmp_path):
+def test_experiment_setup(tmp_path, mocker: MockerFixture):
     _info = EnvironmentInfo(
         profile="test",
         properties=MlflowStorageProperties(
             artifact_location=tmp_path.as_uri(), workspace_directory=f"/Shared/dbx/{tmp_path.name}"
         ),
     )
+
+    setup_mocker = MagicMock()
+    mocker.patch("mlflow.set_experiment", setup_mocker)
     MlflowStorageConfigurationManager._setup_experiment(_info)
-    assert mlflow.get_experiment_by_name(f"/Shared/dbx/{tmp_path.name}") is not None
+    setup_mocker.assert_called_once()
