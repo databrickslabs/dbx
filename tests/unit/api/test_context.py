86d095a4f4754de04db5aef8f351a270a5864350
renardeinside <polarpersonal@gmail.com>
2022-08-20 12:42:25 +0200

    remove temp test artifacts

--- tests/unit/api/test_context.py
+++ tests/unit/api/test_context.py
@@ -2,14 +2,14 @@ from dbx.api.context import LocalContextManager
 from dbx.models.context import ContextInfo
 
 
-def test_local_context_serde():
+def test_local_context_serde(temp_project):
     ctx_id = "aaa111aaabbb"
     LocalContextManager.set_context(ContextInfo(context_id=ctx_id))
     result = LocalContextManager.get_context()
     assert ctx_id == result.context_id
 
 
-def test_local_context_non_existent():
+def test_local_context_non_existent(temp_project):
     LocalContextManager.context_file_path.unlink(missing_ok=True)
     result = LocalContextManager.get_context()
     assert result is None
