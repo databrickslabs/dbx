405a43cf9cd252315093eab81c3f3755afd00c2b
renardeinside <polarpersonal@gmail.com>
2022-08-20 19:19:41 +0200

    fix path reference

--- dbx/api/jinja.py
+++ dbx/api/jinja.py
@@ -7,7 +7,7 @@ from dbx.utils import dbx_echo
 
 def get_last_modified_file(path: str, extension: str) -> Optional[str]:
     file_locator = Path(path).glob(f"*.{extension}")
-    sorted_locator = sorted(file_locator, key=os.path.getmtime)  # get latest modified file, aka latest package version
+    sorted_locator = sorted(file_locator, key=os.path.getmtime)
     if sorted_locator:
         file_path = sorted_locator[-1]
         dbx_echo(f"Found latest file in provided path {path}")
